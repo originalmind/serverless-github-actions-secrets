8a9a42e842e40343b9d58632b5220faae7434493
Joe Niland <joe@originalmind.com.au>
2020-05-03 08:30:48 +1000

    Read default data from package.json

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # serverless-github-actions-secrets
+
 Manage Serveless Framework config values in GitHub Actions Secrets
+
+## Usage
+
+1. Add a script entry to your package.json:
+   ```
+   "scripts: {
+     "secrets": "github-secrets"
+   }
+   ```
+2. Manual use:
+   ```
+   export GITHUB_ACCESS_TOKEN=...
+   npm run secrets -- --stage dev
+   ```
+
+   or token and repo can be supplied:
+
+   ```
+   npm run secrets -- --stage dev --token my_github_token --repo owner/repo-name
+   ```
+
+### Usage with git hook
+
+Since you're using Github Actions, it's a good idea to push the secrets before pushing your code. This can be automated using git hooks.
+
+Since we can't specify a stage, we must map it from the current branch. This is specified in the package.json. This package will read this metadata if the `--stage` option is not specified.
+
+1. `npm i husky --save-dev`
+2. Add required metadata to package.json:
+   ```
+	"husky": {
+		"hooks": {
+		  "pre-push": "github-secrets",
+		}
+	},
+	"om_sgas": {
+      "stage_mapping": {
+		"develop": "dev",
+        "master": "prod"
+      }
+    },
+   ```
+
+   More info about [Husky](https://github.com/typicode/husky).
+
+## Debugging
+
+```
+NODE_DEBUG=app npm run dev
+```
--- index.js
+++ index.js
@@ -1,73 +1,78 @@
-const fs = require('fs'),
-  yaml = require('yaml'),
-  ini = require('ini'),
-  cla = require('command-line-args'),
-  github = require('./lib/github');
+const fs = require("fs"),
+  yaml = require("yaml"),
+  ini = require("ini"),
+  cla = require("command-line-args"),
+  github = require("./lib/github"),
+  packageReader = require("./lib/package-reader"),
+  util = require('util'),
+  debuglog = util.debuglog('app');
 
 const optionDefinitions = [
   {
-    name: 'token',
-    alias: 't',
+    name: "token",
+    alias: "t",
     type: String,
-    description: 'GitHub OAuth2 token',
+    description: "GitHub OAuth2 token",
   },
   {
-    name: 'repo',
-    alias: 'r',
+    name: "repo",
+    alias: "r",
     type: String,
     description: 'GitHub repo path as "owner/repo"',
   },
   {
-    name: 'stage',
-    alias: 's',
+    name: "stage",
+    alias: "s",
     type: String,
-    description: 'Deployment stage, e.g. dev, staging, prod',
+    description: "Deployment stage, e.g. dev, staging, prod",
   },
   {
-    name: 'operation',
-    alias: 'o',
+    name: "operation",
+    alias: "o",
     type: String,
-    description: 'One of: write, list, get, cred',
-    default: 'write',
+    description: "One of: write, list, get, cred",
+    default: "write",
   },
   {
-    name: 'secretName',
-    alias: 'n',
+    name: "secretName",
+    alias: "n",
     type: String,
-    description: 'GitHub secret name',
+    description: "GitHub secret name",
   },
   {
-    name: 'awsProfileName',
-    alias: 'p',
+    name: "awsProfileName",
+    alias: "p",
     type: String,
-    description: 'AWS profile name from credentials file',
+    description: "AWS profile name from credentials file",
   },
 ];
 const options = cla(optionDefinitions);
 
 if (!options.token) {
-  console.error('Token must be specified');
-  process.exit();
+  if (!process.env.GITHUB_ACCESS_TOKEN) {
+    throw new Error("Token must be specified");
+  }
+  options.token = process.env.GITHUB_ACCESS_TOKEN;
 }
 
 if (!options.repo) {
-  console.error('Repo path must be specified');
-  process.exit();
+  options.repo = new packageReader().getRepo();
 }
 
 if (!options.stage) {
-  console.error('Stage must be specified');
-  process.exit();
+  options.stage = new packageReader().getStage();
 }
+
+debuglog(`Running with options: ${options.token} ${options.repo} ${options.stage}`);
 
 const gitHubAPI = new github(options.token, options.repo);
 
 switch (options.operation) {
-  case 'write': {
+  case "write": {
     const configFile = `../../sls/config/config.${options.stage}.secret.yml`;
-    console.log('Reading ' + configFile);
+    console.log("Reading " + configFile);
 
-    const configDoc = yaml.parse(fs.readFileSync(configFile, 'utf8'));
+    const configDoc = yaml.parse(fs.readFileSync(configFile, "utf8"));
 
     Object.keys(configDoc).forEach((configKey) => {
       gitHubAPI.writeToGitHub(configKey, configDoc[configKey]);
@@ -75,35 +80,35 @@ switch (options.operation) {
 
     break;
   }
-  case 'cred': {
+  case "cred": {
     if (!options.awsProfileName) {
       console.error(
-        'AWS profile name must be specified for operation type "cred"',
+        'AWS profile name must be specified for operation type "cred"'
       );
       process.exit();
     }
 
     const configFile = `${process.env.HOME}/.aws/credentials`;
-    console.log('Reading ' + configFile);
+    console.log("Reading " + configFile);
 
-    const configDoc = ini.parse(fs.readFileSync(configFile, 'utf8'));
+    const configDoc = ini.parse(fs.readFileSync(configFile, "utf8"));
 
     gitHubAPI.writeToGitHub(
-      'AWS_ACCESS_KEY_ID',
-      configDoc[options.awsProfileName].aws_access_key_id,
+      "AWS_ACCESS_KEY_ID",
+      configDoc[options.awsProfileName].aws_access_key_id
     );
     gitHubAPI.writeToGitHub(
-      'AWS_SECRET_ACCESS_KEY',
-      configDoc[options.awsProfileName].aws_secret_access_key,
+      "AWS_SECRET_ACCESS_KEY",
+      configDoc[options.awsProfileName].aws_secret_access_key
     );
 
     break;
   }
-  case 'list': {
+  case "list": {
     gitHubAPI.listSecrets();
     break;
   }
-  case 'get': {
+  case "get": {
     if (!options.secretName) {
       console.error('Secret name must be specified for operation type "get"');
       process.exit();
@@ -112,7 +117,7 @@ switch (options.operation) {
     break;
   }
   default: {
-    console.log('No operation specified');
+    console.log("No operation specified");
     break;
   }
 }
